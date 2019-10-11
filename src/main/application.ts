190b24a4a213dceb5b1c276759cd0ea4d1d5d054
Boris Okunskiy <boris@okunskiy.name>
2019-10-11 17:43:40 +0300

    wip: extract HttpServer from Application

--- src/main/application.ts
+++ src/main/application.ts
@@ -1,20 +1,7 @@
-import Koa, { Middleware, Context } from 'koa';
 import { Container } from 'inversify';
-import { RequestLogger } from './logger';
-import { Router, RouterConstructor } from './router';
-import http from 'http';
-import https from 'https';
-import stoppable, { StoppableServer } from 'stoppable';
-import bodyParser from 'koa-bodyparser';
-import conditional from 'koa-conditional-get';
-import etag from 'koa-etag';
-import cors from '@koa/cors';
-import * as middleware from './middleware';
 import { Constructor } from './util';
 import { RequestFactory } from './request';
-import { Exception, Logger, StandardLogger } from '@ubio/essentials';
-
-type AsyncFn = () => Promise<any>;
+import { Logger, StandardLogger } from '@ubio/essentials';
 
 /**
  * Application provides an IoC container where all modules should be registered
@@ -29,37 +16,23 @@ type AsyncFn = () => Promise<any>;
  * Simply avoid invoking `app.startServer()` and manage the app lifecycle separately
  * (e.g invoke `app.runStartHooks()` instead of `app.startServer()`).
  */
-export class Application extends Koa {
+export class Application {
     container: Container;
-    server: StoppableServer | null = null;
-    startHooks: AsyncFn[] = [];
-    stopHooks: AsyncFn[] = [];
 
     constructor() {
-        super();
         const container = new Container({ skipBaseClassChecks: true });
         this.container = container;
-        this.proxy = true;
-        process.once('SIGTERM', () => this.gracefulShutdown('SIGTERM'));
-        process.once('SIGINT', () => this.gracefulShutdown('SIGINT'));
         this.bindSingleton(Logger, StandardLogger);
         this.bind(RequestFactory);
+        // TODO consider adding default environment-based configuration
     }
 
     get logger(): Logger {
         return this.container.get<Logger>(Logger);
     }
 
-    /**
-     * A shortcut to register a router class in IoC container.
-     * Standard routing middleware uses Router constructor
-     * as a service identifier for enlisting all routes, so this
-     * method simply enforces this convention.
-     */
-    bindRouter(routerClass: RouterConstructor): this {
-        this.container.bind(Router).to(routerClass);
-        return this;
-    }
+    async beforeStart(): Promise<void> {}
+    async afterStop(): Promise<void> {}
 
     /**
      * A shortcut to bind a singleton.
@@ -92,141 +65,16 @@ export class Application extends Koa {
         return this;
     }
 
-    createRoutingMiddleware(): Middleware {
-        return async (ctx: Context) => {
-            // Request container injects 'KoaContext' (by string!)
-            // and overrides Logger with RequestLogger.
-            const requestContainer = new Container({ skipBaseClassChecks: true });
-            requestContainer.parent = this.container;
-            requestContainer.bind('KoaContext').toConstantValue(ctx);
-            requestContainer.bind(Logger).to(RequestLogger).inSingletonScope();
-            ctx.container = requestContainer;
-            ctx.logger = requestContainer.get<Logger>(Logger);
-
-            const routers = requestContainer.getAll<Router>(Router);
-            for (const router of routers) {
-                const handled = await router.handle();
-                if (handled) {
-                    return;
-                }
-            }
-
-            throw new Exception({
-                name: 'RouteNotFoundError',
-                status: 404
-            });
-        };
+    async start() {
+        process.once('SIGTERM', () => this.stop());
+        process.once('SIGINT', () => this.stop());
+        await this.beforeStart();
     }
 
-    addStandardMiddleware(): this {
-        this.use(bodyParser({
-            enableTypes: ['json']
-        }));
-        this.use(conditional());
-        this.use(etag());
-        this.use(cors({
-            exposeHeaders: ['Date', 'Content-Length'],
-            maxAge: 15 * 60
-        }));
-        this.use(middleware.debugRequestLog);
-        this.use(middleware.requestId);
-        this.use(middleware.responseTime);
-        this.use(middleware.errorHandler);
-        this.use(this.createRoutingMiddleware());
-        return this;
-    }
-
-    beforeStart(asyncFn: AsyncFn): this {
-        this.startHooks.push(asyncFn);
-        return this;
-    }
-
-    afterStop(asyncFn: AsyncFn): this {
-        this.stopHooks.push(asyncFn);
-        return this;
-    }
-
-    async startServer(port: number) {
-        if (this.server) {
-            return;
-        }
-        const server = stoppable(http.createServer(this.callback()), 10000);
-        this.server = server;
-        await this.runStartHooks();
-        server.listen(port, () => {
-            this.logger.info(`Listening on ${port}`);
-        });
-    }
-
-    async startHttpsServer(port: number, options: https.ServerOptions) {
-        if (this.server) {
-            return;
-        }
-        const server = stoppable(https.createServer(options, this.callback()), 10000);
-        this.server = server;
-        await this.runStartHooks();
-        server.listen(port, () => {
-            this.logger.info(`Listening on ${port}`);
-        });
-    }
-
-    async stopServer() {
-        const server = this.server;
-        if (!server) {
-            return;
-        }
+    async stop() {
+        // TODO uninstall process signals handlers better
         process.removeAllListeners();
-        await new Promise(r => server.stop(r));
-        await this.runStopHooks();
-    }
-
-    async runStartHooks() {
-        for (const asyncFn of this.startHooks) {
-            await asyncFn();
-        }
-    }
-
-    async runStopHooks() {
-        for (const asyncFn of this.stopHooks) {
-            await asyncFn();
-        }
-    }
-
-    async gracefulShutdown(signal: string) {
-        const server = this.server;
-        if (!server) {
-            return;
-        }
-        try {
-            if (process.env.NODE_ENV === 'production') {
-                this.logger.info(`Graceful shutdown: received ${signal}, wait for traffic to stop being sent`);
-                await new Promise(r => setTimeout(r, 10000));
-            }
-            this.logger.info('Graceful shutdown: stop accepting new requests, wait for existing requests to finish');
-            if (process.env.NODE_ENV === 'production') {
-                await this.stopServer();
-            }
-            this.logger.info('Graceful shutdown: complete');
-        } catch (error) {
-            this.logger.error('Graceful shutdown: failed', { error });
-            process.exit(1);
-        }
-    }
-
-    // Experimental
-    generateEndpointDocs() {
-        const container = new Container({ skipBaseClassChecks: true });
-        container.parent = this.container;
-        container.bind('KoaContext').toConstantValue({});
-        const routers = container.getAll<Router>(Router);
-        const doc = {
-            // TODO add more info
-            paths: {}
-        };
-        for (const router of routers) {
-            Object.assign(doc.paths, router.generateDocs());
-        }
-        return doc;
+        await this.afterStop();
     }
 
 }
