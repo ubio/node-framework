d3b4eda9e51799452d7ac8763e9f247b014c8849
Boris Okunskiy <boris@okunskiy.name>
2022-10-26 18:00:51 +0300

    chore: adjust request logging

--- src/main/middleware/request-log.ts
+++ src/main/middleware/request-log.ts
@@ -9,18 +9,15 @@ export async function requestLog(ctx: Context, next: () => Promise<any>) {
     } finally {
         const logger = ctx.logger;
         if (['info', 'debug'].includes(level)) {
-            const latency = ((Date.now() - startedAt) / 1000).toFixed(2);
-            // https://cloud.google.com/logging/docs/reference/v2/rest/v2/LogEntry#HttpRequest
-            logger[level](`${ctx.status} ${ctx.method} ${ctx.path}`, {
-                httpRequest: {
-                    requestMethod: ctx.method,
-                    requestUrl: ctx.path,
-                    status: ctx.status,
-                    referrer: ctx.headers['referer'],
-                    userAgent: ctx.headers['user-agent'],
-                    remoteIp: ctx.ip,
-                    latency,
-                }
+            const latency = (Date.now() - startedAt) + 'ms';
+            logger[level](`HTTP`, {
+                method: ctx.method,
+                url: ctx.url,
+                status: ctx.status,
+                userAgent: ctx.headers['user-agent'],
+                referrer: ctx.headers['referer'],
+                latency,
+                ip: ctx.ip,
             });
         }
     }
