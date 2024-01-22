9ddd49ced8341ef5c906abb92591c202259756ef
Boris Okunskiy <boris@okunskiy.name>
2024-01-22 15:36:26 +0300

    feat: use structured logging (#97)
    * feat: use structured logging
    
    * fix: failing tests
    
    * chore: update typescript to 4.9.5
    
    * stay on old mongodb@4.4.1 strict to avoid unwanted aws dependencies
    
    * use mongodb@4.10.0 (previous package-lock version)
    
    * chore: remove @types/mongodb
    
    ---------
    
    Co-authored-by: inca <boris@okunskiy-name>
    Co-authored-by: Nikolai Abdrafikov <nicck.olay@gmail.com>

--- src/main/logger.ts
+++ src/main/logger.ts
@@ -1,4 +1,4 @@
-import { ConsoleLogger, LOG_LEVELS, LogfmtLogger, Logger, LogLevel } from '@nodescript/logger';
+import { ConsoleLogger, DefaultLogFormatter, LOG_LEVELS, LogfmtFormatter, Logger, LogLevel, LogPayload, StructuredLogFormatter } from '@nodescript/logger';
 import { config } from 'mesh-config';
 
 import { getGlobalMetrics } from './metrics/global.js';
@@ -8,28 +8,27 @@ export {
     LogLevel,
     Logger,
     ConsoleLogger,
-    LogfmtLogger,
 };
 
-export class StandardLogger extends Logger {
+export class StandardLogger extends ConsoleLogger {
 
     @config({ default: 'info' })
     LOG_LEVEL!: string;
     @config({ default: false })
     LOG_PRETTY!: boolean;
-
-    protected delegate: Logger;
+    @config({ default: false })
+    LOG_LOGFMT!: boolean;
 
     constructor() {
         super();
-        this.delegate = this.LOG_PRETTY ? new ConsoleLogger() : new LogfmtLogger();
-        this.delegate.setLevel(this.LOG_LEVEL);
+        this.formatter = this.LOG_PRETTY ? new DefaultLogFormatter() :
+            this.LOG_LOGFMT ? new LogfmtFormatter() : new StructuredLogFormatter();
         this.setLevel(this.LOG_LEVEL);
     }
 
-    override write(level: LogLevel, message: string, data: object): void {
-        getGlobalMetrics().appLogsTotal.incr(1, { severity: level });
-        this.delegate.write(level, message, data);
+    override write(payload: LogPayload) {
+        getGlobalMetrics().appLogsTotal.incr(1, { severity: payload.level });
+        super.write(payload);
     }
 
 }
